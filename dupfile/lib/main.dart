5d2919560bb7a5f177a3f53c06072d9b4e7ecc8b
Arjun Aadhith BS <arjunaadhith2004@gmail.com>
2025-04-06 19:05:25 +0530

    new design added

--- dupfile/lib/main.dart
+++ dupfile/lib/main.dart
@@ -8,6 +8,16 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+
+// import 'package:url_launcher/url_launcher.dart';
+
+// import 'package:url_launcher/url_launcher.dart';
+
+// import 'package:url_launcher/url_launcher.dart';
+
+// import 'package:url_launcher/url_launcher.dart';
+
+
 void main() {
   sqfliteFfiInit();
   databaseFactory = databaseFactoryFfi;
