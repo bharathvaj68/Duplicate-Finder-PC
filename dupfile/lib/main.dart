a89383cd0709b8d7fe5f62bd4d838f705ccb049e
Arjun Aadhith BS <arjunaadhith2004@gmail.com>
2025-04-06 19:01:16 +0530

    new import statement added

--- dupfile/lib/main.dart
+++ dupfile/lib/main.dart
@@ -8,6 +8,11 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+
+import "package:url_launcher/link.dart";
+
+
+
 void main() {
   sqfliteFfiInit();
   databaseFactory = databaseFactoryFfi;
